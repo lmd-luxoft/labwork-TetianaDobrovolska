dc28f2d405546729a985d0bdb3b99f8e1c94fcb1
Tetiana Dobrovolska <TDobrovolska@luxoft.com>
2021-02-19 12:52:34 +0200

    Exercise#9 - OS Features

---
+++ qt-09-os-features/datastreamwriter.cpp
@@ -0,0 +1,24 @@
+#include "datastreamwriter.h"
+
+#include <QFile>
+#include <QDataStream>
+#include <QDebug>
+
+DataStreamWriter::DataStreamWriter(QString fileName, QObject *parent) : QObject(parent)
+{
+    file = new QFile(fileName);
+    file->open(QIODevice::WriteOnly);
+    stream = new QDataStream(file);
+}
+
+DataStreamWriter::~DataStreamWriter()
+{
+    delete stream;
+    delete file;
+}
+
+void DataStreamWriter::writeData(double data)
+{
+    *stream << data;
+    qDebug() << "Write" << data;
+}
